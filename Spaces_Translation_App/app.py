fcf858dfe8ea837e5e4e26423539362f67cab866
Sourcery AI <>
2022-04-25 11:58:28 +0000

    'Refactored by Sourcery'

--- Spaces_Translation_App/app.py
+++ Spaces_Translation_App/app.py
@@ -8,9 +8,7 @@ tokenizer = AutoTokenizer.from_pretrained("t5-base")
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model = AutoModelForSeq2SeqLM.from_pretrained("t5-base")
-        
-    return model
+    return AutoModelForSeq2SeqLM.from_pretrained("t5-base")
 
 model = load_model()
 
@@ -23,7 +21,7 @@ st.title('Simple English ➡️ German/French Translation App')
 st.write('This is a simple machine translation app that will translate\
          your English input text into German or French language\
          by leveraging a pre-trained [Text-To-Text Transfer Tranformers](https://arxiv.org/abs/1910.10683) model.')
-         
+
 st.write('You can see the source code to build this app in the \'Files and version\' tab.')
 
 st.subheader('Input Text')
@@ -31,11 +29,11 @@ text = st.text_area(' ', height=200)
 
 if text != '':
     
-    prefix = 'translate '+str(source_lang)+' to '+str(target_lang)
+    prefix = f'translate {str(source_lang)} to {str(target_lang)}'
     sentence_token =  tokenize.sent_tokenize(text)
     output = tokenizer([prefix+sentence for sentence in sentence_token], padding=True, return_tensors="pt")
     translated_id = model.generate(output["input_ids"], attention_mask=output['attention_mask'], max_length=100)
     translated_word = tokenizer.batch_decode(translated_id, skip_special_tokens=True)
-    
+
     st.subheader('Translated Text')
     st.write(' '.join(translated_word))
